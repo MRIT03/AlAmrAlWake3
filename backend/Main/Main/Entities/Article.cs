ef7f3080b00d3f909603e291a50a0229c5975482
Riad Terek <Riadot03@gmail.com>
2025-04-19 19:04:09 +0300

    Added All the base entities for the database

---
+++ backend/Main/Main/Entities/Article.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Main.Entities;
+
+public class Article
+{
+    [Key]
+    public int ArticleId { get; set; }
+
+    public DateTime TimeCreated { get; set; }
+
+    [Required]
+    public string Headline { get; set; }
+
+    public string Body { get; set; }
+
+    public int PTS { get; set; }
+
+    // Foreign keys
+    public int SourceId { get; set; }
+    public Source Source { get; set; }
+
+    public int RegionId { get; set; }
+    public Region Region { get; set; }
+
+    public ICollection<Reaction> Reactions { get; set; }
+}
