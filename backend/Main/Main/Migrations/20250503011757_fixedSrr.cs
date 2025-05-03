d0add4229f464f85e53dbaec7db7c11990ec7e9f
Riad Terek <Riadot03@gmail.com>
2025-05-03 06:55:58 +0300

    Random

---
+++ backend/Main/Main/Migrations/20250503011757_fixedSrr.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Main.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixedSrr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RSS",
+                table: "Sources");
+
+            migrationBuilder.AddColumn<double>(
+                name: "srr",
+                table: "Sources",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PTS",
+                table: "Articles",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "srr",
+                table: "Sources");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RSS",
+                table: "Sources",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PTS",
+                table: "Articles",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
